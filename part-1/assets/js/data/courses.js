acf63476e30207cb708584c4e9a96550e4fad2bf
All0cator <mpamphs.o.makeris@gmail.com>
2025-11-23 14:16:10 +0200

    .

---
+++ part-1/assets/js/data/courses.js
@@ -0,0 +1,47 @@
+var courses = [
+    {
+        id: "1",
+        img: "assets/img/course_image.png",
+        title: "C++",
+        category: "Προγραμματισμός",
+        semester: "1o έτος",
+        description: "Προγραμματισμός υπολογιστών με C++.",
+        availability: "Διαθέσιμο"
+    },
+    {
+        id: "2",
+        img: "assets/img/course_image.png",
+        title: "Κυβερνοασφάλεια",
+        category: "IT",
+        semester: "2o έτος",
+        description: "Θεωρία ασφάλειας πληροφοριακών συστημάτων.",
+        availability: "Μη Διαθέσιμο"
+    },
+    {
+        id: "3",
+        img: "assets/img/course_image.png",
+        title: "Αρχιτεκτονική Υπολογιστών",
+        category: "Ηλεκτρονικοί Υπολογιστές",
+        semester: "2o έτος",
+        description: "Σχεδίαση CPU, ιεραρχία μνήμης, αποθηκευτικά μέσα.",
+        availability: "Διαθέσιμο"
+    },
+    {
+        id: "4",
+        img: "assets/img/course_image.png",
+        title: "Σχεδίαση Ψηφιακών Συστημάτων",
+        category: "Ηλεκτρονικοί Υπολογιστές",
+        semester: "2o έτος",
+        description: "Ψηφιακά κυκλώματα, λογικές πύλες, εισαγωγή στην ψηφιακή σχεδίαση.",
+        availability: "Διαθέσιμο"
+    },
+    {
+        id: "5",
+        img: "assets/img/course_image.png",
+        title: "Υπολογιστικά Μαθηματικά",
+        category: "Μαθηματικά",
+        semester: "3o έτος",
+        description: "Εισαγωγή στην αριθμητική ανάλυση.",
+        availability: "Διαθέσιμο"
+    }
+]
